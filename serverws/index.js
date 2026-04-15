23693859a5d09c8892a00734e74be9d21eef89ac
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2026-04-15 17:48:10 +0200

    .

---
+++ serverws/index.js
@@ -0,0 +1,21 @@
+const WebSocket = require("ws");
+
+// Creamos servidor en el puerto 8888
+const server = new WebSocket.Server({ port: 8888 });
+
+console.log("Servidor WebSocket activo en puerto 8888");
+
+// Cuando un cliente se conecta
+server.on("connection", (ws) => {
+    console.log("🔌 Cliente conectado");
+
+    // Cuando recibe mensaje
+    ws.on("message", (message) => {
+        console.log("📩 Posición recibida:", message.toString());
+    });
+
+    // Cuando se desconecta
+    ws.on("close", () => {
+        console.log("❌ Cliente desconectado");
+    });
+});
